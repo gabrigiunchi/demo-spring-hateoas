5345c20c75620b449403aeeb066a996ee7ef6eb1
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2020-11-13 13:49:33 +0100

    add properties to HALTemplateProperty.kt

--- src/main/kotlin/com/gabrigiunchi/demohateoas/model/hal/templates/HALTemplateProperty.kt
+++ src/main/kotlin/com/gabrigiunchi/demohateoas/model/hal/templates/HALTemplateProperty.kt
@@ -1,7 +1,13 @@
 package com.gabrigiunchi.demohateoas.model.hal.templates
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 class HALTemplateProperty(
     val name: String,
-    val required: Boolean? = null
+    val required: Boolean? = null,
+    val readOnly: Boolean? = null,
+    val regex: String? = null,
+    val value: String? = null
 )
 
